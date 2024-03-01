b6b940f8299988950423e09b9c9df7d405b95eb4
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-03-01 13:32:38 +0800

    Fix wrong sampling method

--- data/dan_prompt/__init__.py
+++ data/dan_prompt/__init__.py
@@ -1,6 +1,6 @@
 import os
 from datasets import load_dataset
-from random import shuffle, randint, choice
+from random import shuffle, randint, choice, random
 
 
 SELF_FOLDER = os.path.dirname(__file__)
@@ -39,22 +39,33 @@ def generate_prompt(data, target_len="long", tag_seperator=", "):
         target_len = choice(length_map)
     shuffle(data["general"])
 
-    total_target = length_map[target_len] - len(data["special"])
-    input_target = randint(1, length_map[target_len] - 1)
+    total_target = len(data["general"])
+    length = min(length_map[target_len], total_target)
+    input_target = randint(
+        1 * (length // 5),
+        length - 1 * (length // 5),
+    )
 
     prompt_input = data["general"][:input_target]
     prompt_output = data["general"][input_target:total_target]
     generals = data["special"] + prompt_input
-    prompt = f"""
-rating: {tag_seperator.join(data["rating"]) or "<|empty|>"}
-artist: {tag_seperator.join(data["artist"]) or "<|empty|>"}
-characters: {tag_seperator.join(data["character"]) or "<|empty|>"}
-copyrights: {tag_seperator.join(data["copyright"]) or "<|empty|>"}
-target: <|{target_len}|>
-general: {tag_seperator.join(generals)}<|input_end|>
-{tag_seperator.join(prompt_output)}""".strip()
 
-    return prompt
+    rating_str = f"rating: {tag_seperator.join(data['rating']) or '<|empty|>' if random() > 0.7 else '<|empty|>'}"
+    artist_str = f"artist: {tag_seperator.join(data['artist']) or '<|empty|>' if random() > 0.7 else '<|empty|>'}"
+    character_str = f"characters: {tag_seperator.join(data['character']) or '<|empty|>' if random() > 0.7 else '<|empty|>'}"
+    copyright_str = f"copyrights: {tag_seperator.join(data['copyright']) or '<|empty|>' if random() > 0.7 else '<|empty|>'}"
+
+    prior_info = [rating_str, artist_str, character_str, copyright_str]
+    shuffle(prior_info)
+    prior = "\n".join(prior_info)
+
+    user_prompt = f"""{prior}
+target: <|{target_len}|>
+general: {tag_seperator.join(generals)}<|input_end|>""".strip()
+
+    output_prompt = tag_seperator.join(prompt_output)
+
+    return user_prompt, output_prompt
 
 
 def tokenize(tokenizer, prompt, cutoff_len=2048, add_eos_token=True):
@@ -80,16 +91,26 @@ def tokenize(tokenizer, prompt, cutoff_len=2048, add_eos_token=True):
     return result
 
 
-def processor(tokenizer, cutoff_len=512, padding=True):
+def processor(tokenizer, cutoff_len=2048, train_on_inputs=False, padding=True):
     import torch
 
     def generate_and_tokenize_prompt(data_point):
-        prompt = generate_prompt(data_point)
+        user_part, output_part = generate_prompt(data_point)
         tokenized_full_prompt = tokenize(
-            tokenizer, prompt, cutoff_len, add_eos_token=True
+            tokenizer, user_part + output_part, cutoff_len, add_eos_token=True
         )
+        tokenized_user_prompt = tokenize(
+            tokenizer, user_part, cutoff_len, add_eos_token=False
+        )
+        user_prompt_len = len(tokenized_user_prompt["input_ids"])
+        full_prompt_len = len(tokenized_full_prompt["input_ids"])
 
-        pad_len = cutoff_len - len(tokenized_full_prompt["input_ids"])
+        if not train_on_inputs:
+            tokenized_full_prompt["labels"] = [
+                -100
+            ] * user_prompt_len + tokenized_full_prompt["labels"][user_prompt_len:]
+
+        pad_len = cutoff_len - full_prompt_len
         if padding:
             tokenized_full_prompt["input_ids"] = (
                 tokenized_full_prompt["input_ids"] + [0] * pad_len
@@ -109,6 +130,16 @@ def processor(tokenizer, cutoff_len=512, padding=True):
 
 
 if __name__ == "__main__":
+    from transformers import LlamaTokenizer
+    def load_tokenizer(tokenizer_ref="TinyLlama/TinyLlama-1.1B-intermediate-step-480k-1T"):
+        tokenizer = LlamaTokenizer.from_pretrained(tokenizer_ref)
+        apply_special_tokens(tokenizer)
+        tokenizer.pad_token = tokenizer.eos_token
+        return tokenizer
+
+    tokenizer: LlamaTokenizer = load_tokenizer()
     dataset = load("all")
-    data = next(iter(dataset["train"]))
+    data = choice(dataset)
     print(generate_prompt(data, "very_long"))
+    proc = processor(tokenizer, cutoff_len=384)
+    print(proc(data))
